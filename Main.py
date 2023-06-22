60b41e11e51bc6aac624aadffc0f80fb402a4d25
juandavid7777 <juandavid7777@gmail.com>
2023-06-22 10:18:08 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,32 +19,28 @@ st.write("Country:", g.country)
 #--------------------------------
 import streamlit as st
 from flask import request
-import geocoder
 
 def get_client_ip():
     if request:
         return request.remote_addr
     return None
 
+def Main():
+    st.title("Geolocation Capture")
+    st.write("This app captures the geolocation of the user.")
 
-st.title("Geolocation Capture")
-st.write("This app captures the geolocation of the user.")
+    # Get the IP address of the client machine
+    client_ip = get_client_ip()
 
-# Get the IP address of the client machine
-client_ip = get_client_ip()
+    st.write(client_ip)
 
-if client_ip:
-    # Retrieve geolocation information based on the IP address
-    g = geocoder.ip(client_ip)
+    if client_ip:
+        # Display the IP address
+        st.write("Client IP:", client_ip)
+    else:
+        st.write("Unable to retrieve client IP address.")
 
-    # Display the geolocation information
-    st.write("Geolocation:")
-    st.write("Latitude:", g.lat)
-    st.write("Longitude:", g.lng)
-    st.write("City:", g.city)
-    st.write("Country:", g.country)
-else:
-    st.write("Unable to retrieve client IP address.")
+Main()
 
 #Plots map
 df_map = pd.DataFrame({"lat":[g.lat], "lon":[g.lng]})
